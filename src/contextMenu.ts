71dab08f08b2bcd6b077d46252bb881535c2eae3
Alvaro Cavalcanti <alvaro.cavalcanti@wpengine.com>
2024-06-03 13:00:25 +0100

    Add & view sheet

--- src/contextMenu.ts
+++ src/contextMenu.ts
@@ -9,6 +9,7 @@ export function setupContextMenu() {
         icon: "/img/add.svg",
         label: "Add Sheet from Beyond",
         filter: {
+          roles: ["GM"],
           every: [
             { key: "layer", value: "CHARACTER" },
             { key: ["metadata", `${ID}/metadata`], value: undefined },
@@ -17,8 +18,9 @@ export function setupContextMenu() {
       },
       {
         icon: "/img/remove.svg",
-        label: "Remove Location Key",
+        label: "Remove Sheet from Beyond",
         filter: {
+          roles: ["GM"],
           every: [{ key: "layer", value: "CHARACTER" }],
         },
       },
@@ -28,7 +30,9 @@ export function setupContextMenu() {
         (item) => item.metadata[`${ID}/metadata`] === undefined
       );
       if (add) {
-        const characterSheetURL = window.prompt("Enter the character's sheet URL:");
+        const characterSheetURL = window.prompt(
+          "Enter the character's sheet URL:"
+        );
         if (!characterSheetURL) {
           OBR.notification.show("No URL entered", "ERROR");
           return;
@@ -44,7 +48,8 @@ export function setupContextMenu() {
             }
           });
         } catch (error) {
-          console.error("Invalid URL:", characterSheetURL);
+          OBR.notification.show("Invalid URL", "ERROR");
+          return;
         }
       } else {
         OBR.scene.items.updateItems(context.items, (items) => {
@@ -55,23 +60,32 @@ export function setupContextMenu() {
       }
     },
   });
-  // OBR.contextMenu.create({
-  //   id: `${ID}/context-menu-expand`,
-  //   icons: [
-  //     {
-  //       icon: "/img/expand.svg",
-  //       label: "Reveal Location Key",
-  //       filter: {
-  //         every: [{ key: "layer", value: "TEXT" }],
-  //       },
-  //     },
-  //   ],
-  //   onClick(context) {
-  //     track("reveal_location_key");
-  //     analytics.track("reveal_location_key");
-  //     OBR.broadcast.sendMessage(`${ID}/broadcast`, `${context.items[0].id}`, {
-  //       destination: "LOCAL",
-  //     });
-  //   },
-  // });
+  OBR.contextMenu.create({
+    id: `${ID}/context-menu-view`,
+    icons: [
+      {
+        icon: "/img/view.svg",
+        label: "View Sheet from Beyond",
+        filter: {
+          roles: ["GM"],
+          every: [
+            { key: "layer", value: "CHARACTER" },
+            { key: ["metadata", `${ID}/metadata`], value: undefined, operator: "!="},
+          ],
+        },
+      },
+    ],
+    onClick(context, elementId) {
+      const metadata: { characterSheetURL: string } = context.items[0].metadata[
+        `${ID}/metadata`
+      ] as { characterSheetURL: string };
+      OBR.popover.open({
+        id: `${ID}/popover`,
+        url: `${metadata.characterSheetURL}`,
+        height: 800,
+        width: 400,
+        anchorElementId: elementId,
+      });
+    },
+  });
 }
